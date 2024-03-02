a88601cfbfb6cb3fddb265a735e1acfa6f53fd6a
mani <varalalokesh051999@gmail.com>
2024-03-02 02:01:55 -0500

    fist commit

---
+++ spring-jenkins/src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class SpringJenkinsApplicationTests {
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+
+	@Test
+	public void contextLoads() {
+		logger.info("test case exicuting");
+		assertEquals(true,true);
+	}
+
+}
