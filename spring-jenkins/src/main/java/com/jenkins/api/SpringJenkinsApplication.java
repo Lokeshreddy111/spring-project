20e514d7c6c267d678db7814310fc900ef4989d8
mani <varalalokesh051999@gmail.com>
2024-03-02 19:37:31 -0500

    fist commit

--- spring-jenkins/src/main/java/com/jenkins/api/SpringJenkinsApplication.java
+++ spring-jenkins/src/main/java/com/jenkins/api/SpringJenkinsApplication.java
@@ -1,10 +1,11 @@
 package com.jenkins.api;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
@@ -12,7 +13,7 @@ public class SpringJenkinsApplication {
 	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@PostConstruct
-	public void intt() {
+	public void init() {
 		logger.info("aplication started");
 	}
 	public static void main(String[] args) {
